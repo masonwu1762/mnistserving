d7867acaaef9fbf6b7aadadba6b33a6ce84516e7
Mason Wu <mason.w@inwinstack.com>
2021-07-30 22:54:13 +0800

    delete secret key

--- mnistserving/settings.py
+++ mnistserving/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xz2!-9thku2nq67ovk9*emj6us3f(yv3-0n6#6cr72$k*ijh-z'
+SECRET_KEY = '--------------'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
